2796b73c4425d229f908b5c8a1c34387da51089a
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-11-27 12:10:53 -0800

    Added styles

--- react-portfolio/src/components/Portfolio.js
+++ react-portfolio/src/components/Portfolio.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { projects } from "../Data";
+import '../styles/Portfolio.css'
 
 function Portfolio() {
+
+
     return (
         <section>
             <div>
                 <h1>Built Projects</h1>
                 {projects.map((projects) => (
                     <><h3>{projects.title}</h3><div>
+                        <p className="wrap">
                         {projects.description}
+                        </p>
+                        <p>
+                            {projects.link}
+                        </p>
 
-                        {projects.link}
                     </div></>
 
 
